4182b7d6a6bb0abc03f3f9416235f522788a0fc1
Gregory Thomas <gregory@redwerks.org>
2025-03-13 09:08:19 -0700

    updated vite config for build deploy over web component

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: "src/web-component.js",
-      name: "WebCalculator",
-      fileName: "web-calculator",
-      formats: ["es", "umd"]
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM"
-        }
-      }
-    }
-  }
+    outDir: "dist",
+  },
 });
